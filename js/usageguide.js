e7938c9c099f424b9330a003a22306a311fce247
Harsh <kaushikyuraj134@gmail.com>
2023-03-24 22:27:50 +0530

    fixed height of element in faq section

--- js/usageguide.js
+++ js/usageguide.js
@@ -1,12 +1,13 @@
-console.log("Hello World!")
-
+// importing the close btn and answer box
 const closeBtn = document.querySelectorAll('.btn-close')
 const answer = document.querySelectorAll('.answer')
 
+// making them arrays
 const closeBtnArray = Array.from(closeBtn)
 const answerArray = Array.from(answer)
 
 
+// adding closing and opening functionality
 closeBtnArray.forEach((element)=>{
     element.addEventListener('click',()=>{
         element.classList.toggle('rotate-90')
