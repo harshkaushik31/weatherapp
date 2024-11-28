fe6e9373254feb79b0dfa3fb56c175b80eded0ad
Harsh Kaushik <kaushikyuvraj134@gmail.com>
2024-11-28 19:40:52 +0530

    git commit -m "new api of openweather"

--- js/script.js
+++ js/script.js
@@ -1,138 +1,124 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fd9ec9c501msh0ca2720615d60e9p19fafbjsnfdea9c55d7bd',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
+const weatherApiHost = 'api.openweathermap.org/data/2.5';
+const weatherApiKey = '73469646ca382230f7803948961ded39';
 
 const getWeather = (city) => {
-    cityName.innerHTML = city
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-	.then(response => response.json())
-	.then((response) => {
-        
-        console.log(response)
-        cloud_pct.innerHTML = response.cloud_pct
-        // feels_like.innerHTML = response.feels_like
-        humidity.innerHTML = response.humidity
-        humidity2.innerHTML = `${response.humidity}<small class="text-muted fw-light"> %</small>`
-        max_temp.innerHTML = response.max_temp
-        min_temp.innerHTML = response.min_temp
-        sunrise.innerHTML = getTime(response.sunrise)
-        sunset.innerHTML = getTime(response.sunset)
-        temp.innerHTML = response.temp
-        temp2.innerHTML = response.temp
-        wind_degrees.innerHTML = response.wind_degrees
-        wind_speed.innerHTML = response.wind_speed
-        wind_speed2.innerHTML = response.wind_speed
-    })
-	.catch(err => console.error(err));
+    cityName.innerHTML = city;
+    fetch(`https://${weatherApiHost}/weather?q=${city}&appid=${weatherApiKey}&units=metric`)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            cloud_pct.innerHTML = response.clouds.all;
+            // feels_like.innerHTML = response.main.feels_like;
+            humidity.innerHTML = response.main.humidity;
+            humidity2.innerHTML = `${response.main.humidity}<small class="text-muted fw-light"> %</small>`;
+            max_temp.innerHTML = response.main.temp_max;
+            min_temp.innerHTML = response.main.temp_min;
+            sunrise.innerHTML = getTime(response.sys.sunrise);
+            sunset.innerHTML = getTime(response.sys.sunset);
+            temp.innerHTML = response.main.temp;
+            temp2.innerHTML = response.main.temp;
+            wind_degrees.innerHTML = response.wind.deg;
+            wind_speed.innerHTML = response.wind.speed;
+            wind_speed2.innerHTML = response.wind.speed;
+        })
+        .catch(err => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value);
+});
+
+getWeather("Delhi");
+
+// function of weather of Shanghai
+function getWeatherShanghai() {
+    fetch(`https://${weatherApiHost}/weather?q=Shanghai&appid=${weatherApiKey}&units=metric`)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            cloud_pct_shanghai.innerHTML = response.clouds.all;
+            feels_like_shanghai.innerHTML = response.main.feels_like;
+            humidity_shanghai.innerHTML = response.main.humidity;
+            max_temp_shanghai.innerHTML = response.main.temp_max;
+            min_temp_shanghai.innerHTML = response.main.temp_min;
+            sunrise_shanghai.innerHTML = getTime(response.sys.sunrise);
+            sunset_shanghai.innerHTML = getTime(response.sys.sunset);
+            temp_shanghai.innerHTML = response.main.temp;
+            wind_degrees_shanghai.innerHTML = response.wind.deg;
+            wind_speed_shanghai.innerHTML = response.wind.speed;
+        })
+        .catch(err => console.error(err));
 }
 
-
-submit.addEventListener("click",(e)=>{
-    e.preventDefault()
-    getWeather(city.value)
-})
-
-getWeather("Delhi")
-
-// function of weather of shanghai
-function getWeatherShanghai () {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Shanghai', options)
-	.then(response => response.json())
-	.then((response) => {
-        
-        console.log(response)
-        cloud_pct_shanghai.innerHTML = response.cloud_pct
-        feels_like_shanghai.innerHTML = response.feels_like
-        humidity_shanghai.innerHTML = response.humidity
-        max_temp_shanghai.innerHTML = response.max_temp
-        min_temp_shanghai.innerHTML = response.min_temp
-        sunrise_shanghai.innerHTML = getTime(response.sunrise)
-        sunset_shanghai.innerHTML = getTime(response.sunset)
-        temp_shanghai.innerHTML = response.temp
-        wind_degrees_shanghai.innerHTML = response.wind_degrees
-        wind_speed_shanghai.innerHTML = response.wind_speed
-    })
-	.catch(err => console.error(err));
+// function for weather of Boston
+function getWeatherBoston() {
+    fetch(`https://${weatherApiHost}/weather?q=Boston&appid=${weatherApiKey}&units=metric`)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            cloud_pct_boston.innerHTML = response.clouds.all;
+            feels_like_boston.innerHTML = response.main.feels_like;
+            humidity_boston.innerHTML = response.main.humidity;
+            max_temp_boston.innerHTML = response.main.temp_max;
+            min_temp_boston.innerHTML = response.main.temp_min;
+            sunrise_boston.innerHTML = getTime(response.sys.sunrise);
+            sunset_boston.innerHTML = getTime(response.sys.sunset);
+            temp_boston.innerHTML = response.main.temp;
+            wind_degrees_boston.innerHTML = response.wind.deg;
+            wind_speed_boston.innerHTML = response.wind.speed;
+        })
+        .catch(err => console.error(err));
 }
 
-// fuction for weather of boston
-
-function getWeatherBoston () {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Boston', options)
-	.then(response => response.json())
-	.then((response) => {
-        
-        console.log(response)
-        cloud_pct_boston.innerHTML = response.cloud_pct
-        feels_like_boston.innerHTML = response.feels_like
-        humidity_boston.innerHTML = response.humidity
-        max_temp_boston.innerHTML = response.max_temp
-        min_temp_boston.innerHTML = response.min_temp
-        sunrise_boston.innerHTML = getTime(response.sunrise)
-        sunset_boston.innerHTML = getTime(response.sunset)
-        temp_boston.innerHTML = response.temp
-        wind_degrees_boston.innerHTML = response.wind_degrees
-        wind_speed_boston.innerHTML = response.wind_speed
-    })
-	.catch(err => console.error(err));
-}
-
-// fuction for weather of Lucknow
-function getWeatherLucknow () {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Lucknow', options)
-	.then(response => response.json())
-	.then((response) => {
-        
-        console.log(response)
-        cloud_pct_lucknow.innerHTML = response.cloud_pct
-        feels_like_lucknow.innerHTML = response.feels_like
-        humidity_lucknow.innerHTML = response.humidity
-        max_temp_lucknow.innerHTML = response.max_temp
-        min_temp_lucknow.innerHTML = response.min_temp
-        sunrise_lucknow.innerHTML = getTime(response.sunrise)
-        sunset_lucknow.innerHTML = getTime(response.sunset)
-        temp_lucknow.innerHTML = response.temp
-        wind_degrees_lucknow.innerHTML = response.wind_degrees
-        wind_speed_lucknow.innerHTML = response.wind_speed
-    })
-	.catch(err => console.error(err));
+// function for weather of Lucknow
+function getWeatherLucknow() {
+    fetch(`https://${weatherApiHost}/weather?q=Lucknow&appid=${weatherApiKey}&units=metric`)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            cloud_pct_lucknow.innerHTML = response.clouds.all;
+            feels_like_lucknow.innerHTML = response.main.feels_like;
+            humidity_lucknow.innerHTML = response.main.humidity;
+            max_temp_lucknow.innerHTML = response.main.temp_max;
+            min_temp_lucknow.innerHTML = response.main.temp_min;
+            sunrise_lucknow.innerHTML = getTime(response.sys.sunrise);
+            sunset_lucknow.innerHTML = getTime(response.sys.sunset);
+            temp_lucknow.innerHTML = response.main.temp;
+            wind_degrees_lucknow.innerHTML = response.wind.deg;
+            wind_speed_lucknow.innerHTML = response.wind.speed;
+        })
+        .catch(err => console.error(err));
 }
 
 // function to get weather of Kolkata
-function getWeatherKolkata () {
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Kolkata', options)
-	.then(response => response.json())
-	.then((response) => {
-        
-        console.log(response)
-        cloud_pct_kolkata.innerHTML = response.cloud_pct
-        feels_like_kolkata.innerHTML = response.feels_like
-        humidity_kolkata.innerHTML = response.humidity
-        max_temp_kolkata.innerHTML = response.max_temp
-        min_temp_kolkata.innerHTML = response.min_temp
-        sunrise_kolkata.innerHTML = getTime(response.sunrise)
-        sunset_kolkata.innerHTML = getTime(response.sunset)
-        temp_kolkata.innerHTML = response.temp
-        wind_degrees_kolkata.innerHTML = response.wind_degrees
-        wind_speed_kolkata.innerHTML = response.wind_speed
-    })
-	.catch(err => console.error(err));
+function getWeatherKolkata() {
+    fetch(`https://${weatherApiHost}/weather?q=Kolkata&appid=${weatherApiKey}&units=metric`)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            cloud_pct_kolkata.innerHTML = response.clouds.all;
+            feels_like_kolkata.innerHTML = response.main.feels_like;
+            humidity_kolkata.innerHTML = response.main.humidity;
+            max_temp_kolkata.innerHTML = response.main.temp_max;
+            min_temp_kolkata.innerHTML = response.main.temp_min;
+            sunrise_kolkata.innerHTML = getTime(response.sys.sunrise);
+            sunset_kolkata.innerHTML = getTime(response.sys.sunset);
+            temp_kolkata.innerHTML = response.main.temp;
+            wind_degrees_kolkata.innerHTML = response.wind.deg;
+            wind_speed_kolkata.innerHTML = response.wind.speed;
+        })
+        .catch(err => console.error(err));
 }
 
 // calling all the functions initially
-
-getWeatherShanghai()
-getWeatherBoston()
-getWeatherLucknow()
-getWeatherKolkata()
+getWeatherShanghai();
+getWeatherBoston();
+getWeatherLucknow();
+getWeatherKolkata();
 
 // function to convert the API Time to a more readable format
-function getTime(timestamp){
-const time = new Date(timestamp * 1000).toLocaleTimeString();
-
-return time;
+function getTime(timestamp) {
+    const time = new Date(timestamp * 1000).toLocaleTimeString();
+    return time;
 }
